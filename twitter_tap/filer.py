be833436d90258a72781a213b53e4568a778fb9c
Petra <petra.kralj.novak@ijs.si>
2020-03-20 12:27:38 +0100

    Improved writing to files with directory structure year/month/day and parameter number of tweets per file.

--- twitter_tap/filer.py
+++ twitter_tap/filer.py
@@ -8,45 +8,55 @@ import gzip
 
 class filer:
 
-    def __init__(self, dataDir, N=10000):
+    def __init__(self, dataDir, N=500):
         if not os.path.exists(dataDir):
             raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), dataDir)
         self.dataDir = dataDir
-        self.data = []
-        self.N = N
+        self.counter = 0    #number of tweets in a file
+        self.N = N          #max number of tweets in a file
+        self.flushEvery = 100
+        self.newFile()
+
 
     def emit(self, dictEntry):
-        self.data.append(dictEntry)
-        if len(self.data) >= self.N:   # add condition  "if new day"
-            self.toDisc()
+        textToWtite = json.dumps(dictEntry)+ "\n"
+        self.file.write(textToWtite)
+        if self.counter > self.flushEvery:
+            self.file.flush()
+        if self.counter >= self.N:   # add condition  "if new day"
+            self.closeFile()
+            self.newFile()
 
-    def toDisc(self):
-        #generate directory name & create it if id does not exist UTC!
+
+    def newFile(self):
+        #generate directory name & create it if id does not exist, time in UTC!
         now = datetime.datetime.utcnow()
         directory = os.path.join(self.dataDir, now.strftime("%Y/%m/%d"))
         os.makedirs(directory, mode=0o777, exist_ok=True)  # don't raise an error if the directory already exists
 
         #generate filename
-        fileName = now.strftime("%Y-%m-%d_%H-%M-%S")+".json"
+        fileName = now.strftime("%Y-%m-%d_%H-%M-%S")+".txt"
         print(fileName)
 
-        #put tweets to gzipped json file
-        with gzip.GzipFile(os.path.join(directory, fileName + ".gz"), 'w') as outfile:
-            outfile.write(json.dumps(self.data).encode('utf-8'))
+        #open file
+        self.file = open(os.path.join(directory, fileName), 'w')
 
-        #reset the data variable to []
-        self.data = []
 
-    #def __del__(self):
-    #    print(len(self.data))
-     #   self.toDisc()
+    def closeFile(self):
+        self.file.close()
+        self.counter = 0
+
+
+    def __del__(self):
+        self.closeFile()
 
 
 if __name__ == '__main__':
-    f = filer("./data", N=100)
+    f = filer("./data", N=4)
     f.emit({"First" : 1} )
     time.sleep(1)
-    f.emit({"Second": 2,  "this": 77 })
+    f.emit({"Second": 2,  "this": "tralalal"
+                                  "jssjsjsj" })
     time.sleep(1)
     f.emit({"Third": 3, 33: 77})
     time.sleep(1)
@@ -58,6 +68,6 @@ if __name__ == '__main__':
     time.sleep(1)
     f.emit({"Seventh": 7, 777: 77})
 
-    f.toDisc()              # to save on disc the remaining
+
 
 
